37201799d6a8f7adc3fd21a72af6d6adc11e4b2b
Felipe Vidal <contatofelipevidalmendes@gmail.com>
2020-09-26 17:20:25 -0300

    domains felipe

---
+++ Projeto_EDUX/Domains/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_EDUX.Domains
+{
+    public class Categoria : BaseDomains
+    {
+        public string Tipo { get; set; }
+    }
+}
