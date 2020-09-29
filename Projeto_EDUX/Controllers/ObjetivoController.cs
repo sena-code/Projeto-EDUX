af576b00024075b31f6efcb20c72c82ff6d454f9
Felipe Vidal <contatofelipevidalmendes@gmail.com>
2020-09-29 19:45:07 -0300

    teste outros

--- Projeto_EDUX/Controllers/ObjetivoController.cs
+++ Projeto_EDUX/Controllers/ObjetivoController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Projeto_EDUX.Controllers
 {
-    public class ObjetivoController
+    public class ObjetivoController : ControllerBase
     {
     }
 }
