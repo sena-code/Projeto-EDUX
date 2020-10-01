461d06440adf3a1727a38eff293578e2a546f108
Pamsg <61585584+Pamsg@users.noreply.github.com>
2020-10-01 15:10:40 -0300

    Add files via upload

---
+++ Projeto_EDUX/Interfaces/ITurma.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_EDUX.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_EDUX.Interfaces
+{
+    interface ITurma
+    {
+        List<Turma> ListarTodas();
+        Turma BuscarPorID(Guid id);
+        void Adicionar(Turma turma);
+        void Alterar(Guid id);
+        void Editar(Turma Turma);
+        void Excluir(Guid id);
+    }
+}
